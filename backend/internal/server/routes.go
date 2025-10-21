c1295d5db280d0d78819f5b761f55056ecd86884
Raain <ryanbwgt@gmail.com>
2025-10-21 14:38:06 +0200

    fix: Add production URL to CORS allowed origins

--- backend/internal/server/routes.go
+++ backend/internal/server/routes.go
@@ -17,7 +17,7 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Add your frontend URL
+		AllowOrigins:     []string{"http://localhost:5173", "http://169.255.58.102"}, // Dev and production URLs
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"},
 		AllowHeaders:     []string{"Accept", "Authorization", "Content-Type"},
 		AllowCredentials: true, // Enable cookies/auth
